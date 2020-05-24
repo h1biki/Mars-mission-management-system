575a439bb803bfbc22f32df2a85ee7cfbab9ea88
Zixi Zhao <zzha0195@student.monash.edu>
2020-05-24 16:58:53 +1000

    wip

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -6,12 +6,20 @@ public class UserInterface {
     public UserInterface() {
         }
 
-        public void start () {
+        public void start() {
             Input input = new Input();
             String option = "";//select option from the main menu
 
             while (option.equals("2")) {//main loop
-                displayMainMenu();
+                System.out.println("");
+                System.out.println("============================================");
+                System.out.println("*   Welcome To The Mission to Mars System  *");
+                System.out.println("*                                          *");
+                System.out.println("* Please select from the following options *");
+                System.out.println("*            Press 1 to Login              *");
+                System.out.println("*            Press 2 to exit               *");
+                System.out.println("============================================");
+                System.out.println("");
                 option = input.acceptStringInput("Please Choose Your Option: ");
                 switch (option) {
                     case "1":
@@ -24,29 +32,44 @@ public class UserInterface {
             }
         }
 
-        public void displayMainMenu () {
-            System.out.println("");
-            System.out.println("============================================");
-            System.out.println("*   Welcome To The Mission to Mars System  *");
-            System.out.println("*                                          *");
-            System.out.println("* Please select from the following options *");
-            System.out.println("*            Press 1 to Login              *");
-            System.out.println("*            Press 2 to exit               *");
-            System.out.println("============================================");
-            System.out.println("");
-        }
-
-        public void displayLoginMenu () {
+        public void displayLoginMenu() {
 
         }
 
-        public void displayMissionCoordinatorMenu () {
+        public void displayMissionCoordinatorMenu() {
 
         }
 
-        public void displayAdministratorMenu () {
+        public void displayAdministratorMenu() {
 
         }
+
+        public void displayShuttle() {
+
+        }
+
+        public void displayCriteria() {
+
+        }
+
+        public void displayNBestCandidates() {
+
+        }
+
+        public void displayJob() {
+
+        }
+
+        public void displayMission() {
+
+        }
+
+        public void displayException(String string) {
+
+        }
+
+
+
 
 
     }
